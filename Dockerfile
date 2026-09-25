bb58522206db8784edb5bd2f8c1f6579c40841f2
William Wheeler <40714988@live.napier.ac.uk>
2026-09-25 15:45:15 +0100

    Creating a Network Bridge in Docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM amazoncorretto:17
-COPY ./target/classes/com /tmp/com
+COPY ./target/semApp.jar /tmp
 WORKDIR /tmp
-ENTRYPOINT ["java", "com.napier.sem.Main"]
+ENTRYPOINT ["java", "-jar", "semApp.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,4 +1,4 @@
-package com.napier.devops;
+package com.napier.sem;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
@@ -9,8 +9,8 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
